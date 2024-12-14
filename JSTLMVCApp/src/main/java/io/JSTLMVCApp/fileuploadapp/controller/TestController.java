3a918413cb538dfb8d07ee454518de3c3a80d6ca
kiryuchi10 <kaminer_sol2@naver.com>
2024-12-15 03:15:11 +0900

    241215 commit
    mvc model with VO modified for SPRING MVC APPLICATION , encapsulate data and transfer it between layes

---
+++ JSTLMVCApp/src/main/java/io/JSTLMVCApp/fileuploadapp/controller/TestController.java
@@ -0,0 +1,15 @@
+package io.JSTLMVCApp.fileuploadapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping
+    public String testConnection() {
+        return "Backend is connected to the frontend!";
+    }
+}
