109646f199c66d8bcf615afded0b6f4c6f460e9d
kiryuchi10 <kaminer_sol2@naver.com>
2024-12-16 03:00:09 +0900

    241216 commit
    fileuploading implimenting

--- JSTLMVCApp/frontend/src/App.js
+++ JSTLMVCApp/frontend/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-route
 import Login from "./components/Login";
 import FileUpload from "./components/FileUpload";
 import FileList from "./components/FileList";
+import TestConnection from "./components/TestConnection";
 
 const App = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Login />} />
+				<Route path="/api/test" element={<TestConnection />} />
                 <Route path="/upload" element={<FileUpload />} />
                 <Route path="/files" element={<FileList />} />
             </Routes>
